36f3218bb5f6969153ccab715fdb84768702c48c
shamaahlogbi <candlelogbi@outlook.com>
2025-10-04 22:54:30 +0300

    update homepage

--- lib/feature/home/screen/home_screen.dart
+++ lib/feature/home/screen/home_screen.dart
@@ -15,13 +15,6 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.rtl,
-<<<<<<< HEAD
-      child: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            actionsPadding: EdgeInsets.only(left: 19),
-            actions: [
-=======
       child: Scaffold(
           appBar: AppBar(
               surfaceTintColor: Colors.transparent,
@@ -35,7 +28,7 @@ class HomeScreen extends StatelessWidget {
               ),
 
               child: Image(
-                image: AssetImage("assets/images/solar_bell-bold.png"),
+                image: AssetImage("assets/images/solar_bell-bold (1).png"),
               ),
             ),
           ],
@@ -71,51 +64,51 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             children: [
               SizedBox(height: 20),
->>>>>>> chats
-              Container(
-                padding: EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Color(0xff000000).withOpacity(0.2),
-                ),
-
-                child: Image(
-                  image: AssetImage("assets/images/solar_bell-bold.png"),
-                ),
-              ),
-            ],
-            // leading: Text("موقعك الحالي"),
-            title: Padding(
-              padding: EdgeInsets.only(right: 19),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Text(
-                      "موقعك الحالي",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                  Row(
-                    spacing: 5,
-                    children: [
-                      Image(image: AssetImage("assets/images/Group (5).png")),
-                      Text(
-                        "قرطبة ، الرياض",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      Icon(Icons.keyboard_arrow_down_outlined),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          body: DefaultTabController(
-            length: 2,
-            child: Column(
-              children: [
-                SizedBox(height: 20),
+// >>>>>>> chats
+//               Container(
+//                 padding: EdgeInsets.all(5),
+//                 decoration: BoxDecoration(
+//                   borderRadius: BorderRadius.circular(20),
+//                   color: Color(0xff000000).withOpacity(0.2),
+//                 ),
+//
+//                 child: Image(
+//                   image: AssetImage("assets/images/solar_bell-bold.png"),
+//                 ),
+//               ),
+//             ],
+//             // leading: Text("موقعك الحالي"),
+//             title: Padding(
+//               padding: EdgeInsets.only(right: 19),
+//               child: Column(
+//                 children: [
+//                   Align(
+//                     alignment: Alignment.centerRight,
+//                     child: Text(
+//                       "موقعك الحالي",
+//                       style: TextStyle(color: Colors.black),
+//                     ),
+//                   ),
+//                   Row(
+//                     spacing: 5,
+//                     children: [
+//                       Image(image: AssetImage("assets/images/Group (5).png")),
+//                       Text(
+//                         "قرطبة ، الرياض",
+//                         style: TextStyle(color: Colors.black),
+//                       ),
+//                       Icon(Icons.keyboard_arrow_down_outlined),
+//                     ],
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//           body: DefaultTabController(
+//             length: 2,
+//             child: Column(
+//               children: [
+//                 SizedBox(height: 20),
                 Container(
                   width: double.infinity,
                   padding: EdgeInsets.only(right: 20),
@@ -329,7 +322,7 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
           ),
-        ),
+        // ),
       ),
     );
   }
